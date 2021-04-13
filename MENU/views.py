4d4dd7e7e288ca3a5a3addcc016476abe1211c37
daniel-root <albuquerquedaniel898@gmail.com>
2021-04-12 22:00:20 -0300

    Atualização das views com html

--- MENU/views.py
+++ MENU/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def home(request):
-    return HttpResponse("home")
+def home(request,template='home.html'):
+    return render(request, template)
 
-def perfil(request):
-    return HttpResponse("perfil")
+def perfil(request,template='perfil.html'):
+    return render(request, template)
